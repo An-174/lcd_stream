7f5f5eafbe1730ce612a4023ed99a4a27b0a11ac
An Priyatna <53985855+An-174@users.noreply.github.com>
2019-08-22 19:53:37 +0700

    Update lcd_reg.h

--- lcd_reg.h
+++ lcd_reg.h
@@ -1,3 +1,16 @@
+/*Written by An Priyatna
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+ */
+
 #ifndef LCD_REG_HEADER_
 #define LCD_REG_HEADER_
 #include <avr/io.h>
